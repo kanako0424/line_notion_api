769fbfc8d9618e8043137af7d34030da8476ea29
kanako0424 <budoukan0424@gmail.com>
2024-02-23 19:58:22 +0900

    app.run

--- app.py
+++ app.py
@@ -48,6 +48,6 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 3000))
-    app.run(host="0.0.0.0", port=port)
+   app.run()
+    # port = int(os.getenv("PORT", 3000))
+    # app.run(host="0.0.0.0", port=port)
